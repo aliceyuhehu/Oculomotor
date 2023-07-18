c035e9c4fc3c63d413f02b9bbe8e12f4cc4d38d3
Alice Hu <yh353@duke.edu>
2023-07-17 20:23:17 -0400

    Initial Commit

---
+++ Oculomotor/OculomotorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OculomotorApp.swift
+//  Oculomotor
+//
+//  Created by Alice on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct OculomotorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
